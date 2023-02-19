f7108a47b41e4d86d437c7eebfac321396aa0671
DHANRAJ <61381985+dhanrajdeeru@users.noreply.github.com>
2023-02-19 22:30:17 +0530

    Add files via upload

---
+++ condition.php
@@ -0,0 +1,145 @@
+if($name == 'T1114.001')
+{
+
+}
+else if($name == 'T1219')
+{
+
+}
+else if($name == 'TOO3.001')
+{
+
+}
+else if($name == 'TOO3.002')
+{
+
+}
+else if($name == 'TOO3.003')
+{
+
+}
+else if($name == 'T1552.001')
+{
+
+}
+else if($name == 'T1552.002')
+{
+
+}
+else if($name == 'T1552.006')
+{
+
+}
+else if($name == 'T1555')
+{
+
+}
+else if($name == 'T1555.003')
+{
+
+}
+else if($name == 'T1036.003')
+{
+
+}
+else if($name == 'T1564.001')
+{
+
+}
+else if($name == 'T1564.002')
+{
+
+}
+else if($name == 'T1564.003')
+{
+
+}
+else if($name == 'T1007')
+{
+
+}
+else if($name == 'T1012')
+{
+
+}
+else if($name == 'T1049')
+{
+
+}
+else if($name == 'T1069.001')
+{
+
+}
+else if($name == 'T1087.001')
+{
+
+}
+else if($name == 'T1047')
+{
+
+}
+else if($name == 'T1053.005')
+{
+
+}
+else if($name == 'T1059.003')
+{
+
+}
+else if($name == 'T1106')
+{
+
+}
+else if($name == 'T1569.002')
+{
+
+}
+else if($name == 'T1489')
+{
+
+}
+else if($name == 'T1490')
+{
+
+}
+else if($name == 'T1531')
+{
+
+}
+else if($name == 'T1078.001')
+{
+
+}
+else if($name == 'T1133')
+{
+
+}
+else if($name == 'T1566.001')
+{
+
+}
+else if($name == 'T1137.002')
+{
+
+}
+else if($name == 'T1137.006')
+{
+
+}
+else if($name == 'T1197')
+{
+
+}
+else if($name == 'T1055.001')
+{
+
+}
+else if($name == 'T1134.002')
+{
+
+}
+else if($name == 'T1134.004')
+{
+
+}
+
---
+++ conditionLINUX.php
@@ -0,0 +1,64 @@
+T1564.001if($name == 'T1003.008
+')
+{
+
+}
+else if($name == 'T1007
+')
+{
+
+}
+else if($name == 'T1036.003
+')
+{
+
+}
+else if($name == 'T1049
+')
+{
+
+}
+else if($name == 'T1053.003
+')
+{
+
+}
+else if($name == 'T1056.001
+')
+{
+
+}
+else if($name == 'T1059.004
+')
+{
+
+}
+else if($name == 'T1069.001
+')
+{
+
+}
+else if($name == 'T1087.001
+')
+{
+
+}
+else if($name == 'T1496
+')
+{
+
+}
+else if($name == 'T1552.001
+')
+{
+
+}
+else if($name == 'T1555.003
+')
+{
+
+}
+else if($name == 'T1556.003')
+{
+
+}
